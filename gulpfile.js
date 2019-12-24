812ca010802b04d296db040e8c4dcd732b7fa129
mitelak <mitelak@gmail.com>
2019-12-24 16:00:16 +0100

    Use sass, refactor index and gallery page

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,36 @@
 const { src, dest, watch, series } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+
+sass.compiler = require('sass');
 
 const SOURCE = {
-  CSS: 'static/css/*.css'
+  CSS: 'static/css/*.css',
+  SCSS: '_sass/*.scss'
 }
 
 const OUTPUT = {
-  CSS: '_site/static/css'
+  CSS: '_site/static/css',
+  SCSS: '_site/static/css'
 }
 
 const minCSS = () => {
   return src(SOURCE.CSS)
+    .pipe(concat('styles.min.css'))
     .pipe(cleanCSS({}))
-    .pipe(rename({ extname: '.min.css' }))
     .pipe(dest(OUTPUT.CSS));
 }
 
+const compileSass = () => {
+  return src(SOURCE.SCSS)
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(dest(OUTPUT.SCSS));
+};
+
 exports.watch = () => {
-  watch(SOURCE.CSS, { ignoreInitial: false }, minCSS);
+  watch(SOURCE.SCSS, { ignoreInitial: false }, compileSass);
 }
-exports.default = minCSS
+
+exports.default = compileSass
