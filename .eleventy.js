98d5d18b3753b5a5793900770263f893b2672513
Mitelak <mitelak@gmail.com>
2021-02-12 00:26:43 +0100

    Update menu, move menu to json

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,37 @@
-const CleanCSS = require('clean-css');
+const CleanCSS = require("clean-css");
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addFilter('cssmin', (code) => {
+  eleventyConfig.addFilter("cssmin", (code) => {
     return new CleanCSS({}).minify(code).styles;
   });
 
-  eleventyConfig.addHandlebarsShortcode('removeWhitespaces', (value) => value.replace(/\s/g,''));
-  eleventyConfig.addHandlebarsShortcode('getActive', (arg1, arg2) => arg1 == arg2 ? 'navigation__item--active' : '');
+  eleventyConfig.addHandlebarsShortcode("removeWhitespaces", (value) =>
+    value.replace(/\s/g, "")
+  );
+  eleventyConfig.addHandlebarsShortcode("getActive", (arg1, arg2) =>
+    arg1 == arg2 ? "navigation__item--active" : ""
+  );
+  eleventyConfig.addHandlebarsShortcode("add", (a, b) => a + b);
+  eleventyConfig.addHandlebarsShortcode("ifIsNumber", (value, body) =>
+    typeof value === "number" ? body : ""
+  );
 
   return {
     dir: {
-      input: '.',
-      includes: '_includes',
-      output: '_site'
+      input: ".",
+      includes: "_includes",
+      output: "_site",
     },
     templateFormats: [
-			"hbs",
+      "hbs",
       "png",
       "jpg",
       "gif",
       "jpeg",
       "js",
       "htaccess",
-      "xml"
-		],
-		passthroughFileCopy: true,
+      "xml",
+    ],
+    passthroughFileCopy: true,
   };
 };
