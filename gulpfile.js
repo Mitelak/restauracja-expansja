60394e6b2bcd2ef19eb72bcd0b4eb8b7f06f6025
Mateusz Mitelski <mitelak@gmail.com>
2023-07-28 11:02:48 +0200

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,22 @@
-const { src, dest, watch, series } = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
-
-sass.compiler = require('sass');
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 const SOURCE = {
-  CSS: 'static/css/*.css',
-  SCSS: '_sass/*.scss'
-}
+  SCSS: "_sass/*.scss",
+};
 
 const OUTPUT = {
-  CSS: '_site/static/css',
-  SCSS: '_site/static/css'
-}
-
-const minCSS = () => {
-  return src(SOURCE.CSS)
-    .pipe(concat('styles.min.css'))
-    .pipe(cleanCSS({}))
-    .pipe(dest(OUTPUT.CSS));
-}
+  SCSS: "_site/static/css",
+};
 
 const compileSass = () => {
   return src(SOURCE.SCSS)
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass.sync({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(dest(OUTPUT.SCSS));
 };
 
 exports.watch = () => {
   watch(SOURCE.SCSS, { ignoreInitial: false }, compileSass);
-}
+};
 
-exports.default = compileSass
+exports.default = compileSass;
--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
 (function () {
-  var galleryItems = document.querySelectorAll('[data-gallery]');
-  var galleryModal = document.getElementById('gallery-modal');
-  var galleryModalImage = document.getElementById('gallery-modal-image');
+  var galleryItems = document.querySelectorAll("[data-gallery]");
+  var galleryModal = document.getElementById("gallery-modal");
+  var galleryModalImage = document.getElementById("gallery-modal-image");
 
-  galleryModal.addEventListener('click', function () {
-    galleryModal.classList.remove('gallery-modal-active');
+  galleryModal.addEventListener("click", function () {
+    galleryModal.classList.remove("gallery-modal-active");
   });
 
-  window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      galleryModal.classList.remove('gallery-modal-active');
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      galleryModal.classList.remove("gallery-modal-active");
     }
   });
 
   galleryItems.forEach(function (node) {
-    node.addEventListener('click', function (event) {
+    node.addEventListener("click", function (event) {
       event.preventDefault();
       galleryModalImage.src = event.currentTarget.href;
-      galleryModal.classList.add('gallery-modal-active');
+      galleryModal.classList.add("gallery-modal-active");
     });
   });
 })();
